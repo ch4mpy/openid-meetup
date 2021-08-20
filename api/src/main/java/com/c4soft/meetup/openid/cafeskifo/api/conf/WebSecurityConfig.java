49784062810ad1b4d78af700b802ad19f8980c50
ch4mpy <ch4mp@c4-soft.com>
2021-08-19 16:09:21 -1000

    Auth0 instead of Keycloak

--- api/src/main/java/com/c4soft/meetup/openid/cafeskifo/api/conf/WebSecurityConfig.java
+++ api/src/main/java/com/c4soft/meetup/openid/cafeskifo/api/conf/WebSecurityConfig.java
@@ -1,8 +1,8 @@
 package com.c4soft.meetup.openid.cafeskifo.api.conf;
 
-import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpHeaders;
@@ -12,25 +12,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.c4_soft.springaddons.security.oauth2.keycloak.KeycloakOidcIdAuthenticationConverter;
+import com.c4_soft.springaddons.security.oauth2.SynchronizedJwt2GrantedAuthoritiesConverter;
 import com.c4_soft.springaddons.security.oauth2.oidc.OidcIdAuthenticationToken;
+import com.c4_soft.springaddons.security.oauth2.oidc.SynchronizedJwt2OidcIdAuthenticationConverter;
+import com.nimbusds.jose.shaded.json.JSONArray;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	Converter<Jwt, Collection<GrantedAuthority>> authoritiesConverter;
+	SynchronizedJwt2GrantedAuthoritiesConverter authoritiesConverter() {
+		return (Jwt jwt) -> {
+			final var roles = Optional.ofNullable((JSONArray) jwt.getClaims().get("https://manage.auth0.com/roles")).orElse(new JSONArray());
+
+			return roles.stream().map(Object::toString).map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
+		};
+	}
 
 	@Bean
 	public Converter<Jwt, OidcIdAuthenticationToken> authenticationConverter() {
-		return new KeycloakOidcIdAuthenticationConverter(authoritiesConverter);
+		return new SynchronizedJwt2OidcIdAuthenticationConverter(authoritiesConverter());
 	}
 
 	@Override
@@ -67,14 +74,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/orders/**")
-						.allowedOrigins(
-								"http://localhost",
-								"https://localhost",
-								"https://ch4mpy-bravo:8100",
-								"https://ch4mpy-bravo:4200",
-								"https://cafe-skifo.azurewebsites.net")
-						.allowedMethods("*")
-						.exposedHeaders("Origin", "Accept", "Content-Type", "Location");
+						.allowedOrigins("http://localhost", "https://localhost", "https://ch4mpy-bravo:8100",
+								"https://ch4mpy-bravo:4200", "https://cafe-skifo.azurewebsites.net")
+						.allowedMethods("*").exposedHeaders("Origin", "Accept", "Content-Type", "Location");
 			}
 
 			@Override
